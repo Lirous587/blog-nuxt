bb8f04c259277ce35eb5c506ca4558e733ec35ea
lzh <2115883273@qq.com>
2024-10-14 11:03:52 +0800

    api逻辑bug修复

---
+++ composables/clientApi.js
@@ -0,0 +1,87 @@
+// 不需要进行服务端渲染
+const apiCore = async (url, opt) => {
+  const config = useRuntimeConfig();
+
+  const nuxtApp = useNuxtApp();
+
+  const requestUrl = config.public.apiBase + url;
+
+  return await $fetch(requestUrl, {
+    method: opt.method || "get",
+    onRequest({ options }) {
+      let token = "";
+      token = useCookie("auth_token").value;
+      if (token) {
+        options.headers = {
+          Authorization: `Bearer ${token}`,
+          ...options.headers,
+        };
+      }
+    },
+    // onRequestError({ request, options, error }) {
+    //   // Handle the request errors
+    //   console.log("onRequestError" + error);
+    // },
+    onResponse({ request, response, options }) {
+      // Process the response data
+      if (response.status >= 200 && response.status <= 300) {
+        // console.log(response._data);
+      }
+    },
+    onResponseError({ request, response, options }) {
+      // Handle the response errors
+      if (response._data.msg === "需要登录") {
+        nuxtApp.runWithContext(() => {
+          navigateTo("/login");
+        });
+      }
+
+      if (import.meta.client) {
+        ElMessage({
+          message: response?._data.msg || "未知错误",
+          type: "error",
+        });
+      }
+    },
+    ...opt,
+  });
+};
+
+const commonApi = (method, url, options) => {
+  return new Promise((resolve, reject) => {
+    apiCore(url, {
+      method,
+      ...options,
+    })
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err.data.msg || "请求错误");
+      });
+  });
+};
+
+export const clientApi = {
+  get(url, options) {
+    return commonApi("GET", url, options);
+  },
+  post(url, form, options = {}) {
+    return commonApi("POST", url, {
+      body: form,
+      ...options,
+    });
+  },
+  put(url, form, options = {}) {
+    return commonApi("PUT", url, {
+      body: form,
+      ...options,
+    });
+  },
+  delete(url, form, options = {}) {
+    return commonApi("DELETE", url, {
+      body: form,
+      ...options,
+    });
+  },
+};
---
+++ composables/serverApi.js
@@ -0,0 +1,86 @@
+// 需要进行服务端渲染
+const apiCore = async (url, opt) => {
+  const config = useRuntimeConfig();
+
+  const nuxtApp = useNuxtApp();
+
+  return await useFetch(url, {
+    baseURL: config.public.apiBase,
+    retry: false,
+    onRequest({ options }) {
+      let token = "";
+
+      token = useCookie("auth_token").value;
+      if (token) {
+        options.headers = {
+          Authorization: `Bearer ${token}`,
+          ...options.headers,
+        };
+      }
+    },
+    // onRequestError({ request, options, error }) {
+    //   // Handle the request errors
+    //   console.log("onRequestError" + error);
+    // },
+    onResponse({ request, response, options }) {
+      // Process the response data
+      if (response.status >= 200 && response.status <= 300) {
+        // console.log(response._data);
+      }
+    },
+    onResponseError({ request, response, options }) {
+      // Handle the response errors
+      if (response._data.msg === "需要登录") {
+        nuxtApp.runWithContext(() => {
+          navigateTo("/login");
+        });
+      }
+
+      if (import.meta.client) {
+        ElMessage({
+          message: response?._data.msg || "未知错误",
+          type: "error",
+        });
+      }
+    },
+    ...opt,
+  });
+};
+
+const commonApi = (method, url, options) => {
+  return new Promise((resolve, reject) => {
+    apiCore(url, {
+      method,
+      ...options,
+    }).then((res) => {
+      if (res.error.value) {
+        reject(res.error.value.data);
+      }
+      resolve(res.data.value);
+    });
+  });
+};
+
+export const api = {
+  get(url, options) {
+    return commonApi("GET", url, options);
+  },
+  post(url, form, options = {}) {
+    return commonApi("POST", url, {
+      body: form,
+      ...options,
+    });
+  },
+  put(url, form, options = {}) {
+    return commonApi("PUT", url, {
+      body: form,
+      ...options,
+    });
+  },
+  delete(url, form, options = {}) {
+    return commonApi("DELETE", url, {
+      body: form,
+      ...options,
+    });
+  },
+};
