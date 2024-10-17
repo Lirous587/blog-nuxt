433d56fee0bbce0678cbd2654f91432fc10f8a8b
lzh <2115883273@qq.com>
2024-10-17 17:55:43 +0800

    文章列表查询功能封装

--- composables/util.js
+++ composables/util.js
@@ -1,3 +1,5 @@
+import { getEssayList } from "~/api/user";
+
 // 将query参数转为url
 export function queryToUrl(query) {
   let q = [];
@@ -13,4 +15,36 @@ export function queryToUrl(query) {
 
 export function formateDate(timeStr) {
   return timeStr.split("Z")[0].split("T").join(" ");
+}
+
+export function useCommonGetEssayList(queryForm) {
+  const essayList = ref([]);
+  const loading = ref(false);
+  const totalPage = ref(1);
+
+  const form = reactive({});
+
+  for (const key in queryForm) {
+    form[key] = queryForm[key];
+  }
+
+  const getList = async () => {
+    await getEssayList(form)
+      .then((res) => {
+        const data = res.data;
+        essayList.value = data.list;
+        totalPage.value = data.totalPages;
+      })
+      .finally(() => {
+        loading.value = true;
+      });
+  };
+
+  return {
+    getList,
+    form,
+    essayList,
+    loading,
+    totalPage,
+  };
 }
