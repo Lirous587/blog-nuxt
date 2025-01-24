649117ca81c4e111312a0495f5b2b17fe7810d1b
lzh <2115883273@qq.com>
2025-01-24 16:55:50 +0800

    revise essay-kind moudel

---
+++ api/essay_kind.js
@@ -0,0 +1,16 @@
+export function getEssayKindList(query = {}) {
+  let q = queryToUrl(query);
+  return api.get(`/essay/kind${q}`);
+}
+
+export function createEssayKind(form) {
+  return clientApi.post(`/essay/kind`, form);
+}
+
+export function updateEssayKind(id, form) {
+  return clientApi.put(`/essay/kind/${id}`, form);
+}
+
+export function deleteEssayKind(id) {
+  return clientApi.delete(`/essay/kind/${id}`);
+}
--- pages/admin/kind/index.vue
+++ pages/admin/kind/index.vue
@@ -1,100 +1,102 @@
 <template>
   <div>
-    <el-card>
+    <el-card shadow="always">
       <template #header>
-        <el-button type="primary" @click="drawerRef.open()">添加</el-button>
-      </template>
-      <el-table :data="list" border v-loading="tableLoading">
-        <el-table-column label="id" prop="id"></el-table-column>
-        <el-table-column label="标签名" min-width="120" align="center">
-          <template #default="scope">
-            <el-input size="large" v-model="scope.row.name"></el-input>
-          </template>
-        </el-table-column>
-
-        <el-table-column label="标签介绍" min-width="150" align="center">
-          <template #default="scope">
+        <AdminSearch @search="getData" @reset="resetSearchForm">
+          <template #default>
             <el-input
-              placeholder="请输入标签介绍"
-              v-model="scope.row.introduction"
-              type="textarea"
-              :rows="2"
-            >
-            </el-input>
+              placeholder="标签名"
+              v-model="searchForm.keyword"
+              @keydown.enter="getData"
+            ></el-input>
           </template>
+        </AdminSearch>
+        <el-button type="primary" @click="handelCreate" size="small"
+          >添加
+        </el-button>
+      </template>
+      <el-table :data="tableData" border v-loading="loading">
+        <el-table-column
+          label="id"
+          prop="id"
+          width="120"
+          align="center"
+        ></el-table-column>
+        <el-table-column label="名称" prop="name" align="center">
         </el-table-column>
-
+        <el-table-column label="介绍" prop="introduction" align="center">
+        </el-table-column>
         <el-table-column label="图标" width="180" align="center">
           <template #default="scope">
-            <ChooseIcon v-model:icon="scope.row.icon"></ChooseIcon>
+            <el-icon>
+              <component :is="scope.row.icon" />
+            </el-icon>
           </template>
         </el-table-column>
-        <el-table-column label="文章数" prop="essayCount" align="center" />
-        <el-table-column
-          label="操作"
-          prop="icon"
-          align="center"
-          min-width="200"
-        >
+        <el-table-column label="文章数" prop="essayCount" align="center">
+        </el-table-column>
+        <el-table-column label="操作" prop="icon" align="center" width="180">
           <template #default="scope">
-            <el-button
-              type="warning"
-              :loading="scope.row.loading"
-              @click="handelEdit(scope.row)"
-              >修改</el-button
-            >
+            <el-button type="warning" @click="handelEdit(scope.row)"
+              >修改
+            </el-button>
 
             <el-popconfirm
-              title="确定删除该分类?"
+              title="确定删除?"
               confirm-button-text="确定"
               confirm-button-type="danger"
               cancel-button-text="取消"
               cancel-button-type="primary"
               icon-color="rgb(245,108,108)"
-              @confirm="handelDelete(scope.row)"
+              @confirm="handelDelete(scope.row.id)"
             >
               <template #reference>
-                <el-button type="danger">删除</el-button>
+                <el-button type="danger">删除 </el-button>
               </template>
             </el-popconfirm>
           </template>
         </el-table-column>
       </el-table>
+
+      <template #footer>
+        <div class="mt-5 flex justify-center">
+          <el-pagination
+            background
+            layout="prev, pager,next"
+            :current-page="currentPage"
+            @current-change="getData"
+            :total="pages"
+            :page-count="pages"
+          />
+        </div>
+      </template>
     </el-card>
 
     <MyDrawer
-      ref="drawerRef"
-      title="添加"
+      :title="drawerTitle"
       direction="rtl"
+      ref="drawerRef"
       size="50%"
+      :destroy-on-close="true"
       class="dark:bg-black"
+      @submit="handelSubmit"
     >
-      <el-form :model="form" label-width="80px" :inline="false">
-        <el-form-item label="分类名">
-          <el-input placeholder="请输入分类名称" v-model="form.name"></el-input>
+      <el-form :model="form" ref="formRef" label-width="80px" :inline="false">
+        <el-form-item label="名称" prop="content">
+          <el-input placeholder="请输入名称" size="large" v-model="form.name">
+          </el-input>
         </el-form-item>
-        <el-form-item label="分类介绍">
+        <el-form-item label="介绍">
           <el-input
-            placeholder="请输入标签介绍"
+            placeholder="请输入介绍"
             v-model="form.introduction"
             type="textarea"
             :rows="3"
           >
           </el-input>
         </el-form-item>
-        <el-form-item label="图标">
+        <el-form-item label="图标" prop="content">
           <ChooseIcon v-model:icon="form.icon"></ChooseIcon>
-        </el-form-item>
-        <el-form-item>
-          <el-button
-            type="primary"
-            size="large"
-            @click="handelCreate"
-            class="mt-5 w-full"
-            :loading="loading"
-          >
-            添加</el-button
-          >
         </el-form-item>
       </el-form>
     </MyDrawer>
@@ -102,79 +104,54 @@
 </template>
 
 <script setup>
-import { createKind, deleteKind, updateKind } from "~/api/kind";
-import { useMyAdminStore } from "~/store/admin";
+import {
+  getEssayKindList,
+  createEssayKind,
+  deleteEssayKind,
+  updateEssayKind,
+} from "~/api/essay_kind";
 
 definePageMeta({
   layout: "admin",
-  middleware: "admin",
 });
 
-const form = reactive({
-  name: "",
-  icon: "House",
-  introduction: "",
+//  table
+const {
+  searchForm,
+  resetSearchForm,
+  tableData,
+  loading,
+  currentPage,
+  pages,
+  getData,
+  handelDelete,
+} = useInitTable({
+  getList: getEssayKindList,
+  delete: deleteEssayKind,
+  searchForm: reactive({
+    page: 1,
+    limit: 5,
+    keyword: "",
+  }),
 });
 
-const adminStore = useMyAdminStore();
-
-const drawerRef = ref(null);
-
-const tableLoading = ref(false);
-const loading = ref(false);
-
-const list = ref([]);
-
-const getKindList = () => {
-  const kindList = adminStore.getKindList();
-  if (Array.isArray(kindList)) {
-    list.value = kindList.map((o) => {
-      return {
-        ...o,
-        loading: false,
-      };
-    });
-  }
-};
-getKindList();
-
-const handelCreate = () => {
-  loading.value = true;
-  tableLoading.value = true;
-  createKind(form)
-    .then(async () => {
-      toast("创建成功");
-      drawerRef.value.close();
-      await adminStore.updateAll();
-      getKindList();
-      tableLoading.value = false;
-    })
-    .finally(() => {
-      loading.value = false;
-    });
-};
-
-const handelEdit = (row) => {
-  row.loading = true;
-  updateKind(row)
-    .then(() => {
-      toast("修改成功");
-    })
-    .finally(() => {
-      row.loading = false;
-    });
-};
-
-const handelDelete = (row) => {
-  tableLoading.value = true;
-  deleteKind(row.id)
-    .then(async () => {
-      toast("删除成功");
-      await adminStore.updateAll();
-      getKindList();
-    })
-    .finally(() => {
-      tableLoading.value = false;
-    });
-};
+// form
+const {
+  drawerRef,
+  form,
+  formRef,
+  drawerTitle,
+  handelSubmit,
+  handelCreate,
+  handelEdit,
+} = useInitForm({
+  form: reactive({
+    name: "",
+    icon: "",
+    introduction: "",
+  }),
+  getData,
+  create: createEssayKind,
+  update: updateEssayKind,
+});
 </script>
